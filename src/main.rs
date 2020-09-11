4cf10a9d4a465bc6aecee389e6211b69205cf73f
Max1Truc <maxitruc.bonbec@gmail.com>
2020-09-11 10:01:31 +0200

    Now working!

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,104 @@
-use std::fs::File;
-use std::io::Read;
-
-use pretty_hex::*;
+use flate2::{Decompress, FlushDecompress};
+use std::{
+    cmp::min,
+    fs::File,
+    io::{Read, Write},
+};
 
 const ZLIB_HEADER: [u8; 2] = [0x78, 0x9C]; // https://stackoverflow.com/a/17176881/9438168
+const ZLIB_START: [u8; 6] = [0x00, 0x00, 0x01, 0x00, ZLIB_HEADER[0], ZLIB_HEADER[1]];
+const ZLIB_STOP: [u8; 4] = [0x00, 0x00, 0xFF, 0xFF];
+
+fn find_matching_buffer(
+    base_buffer: &Vec<u8>,
+    sub_buffer: &Vec<u8>,
+    start_at_index: usize,
+) -> Option<usize> {
+    let mut matching_bytes = 0;
+    let mut index = 0;
+
+    let start_index = base_buffer.iter().position(|e| {
+        if index < start_at_index {
+            index += 1;
+            return false;
+        }
+
+        index += 1;
+
+        if sub_buffer[matching_bytes] == *e {
+            matching_bytes += 1;
+
+            if matching_bytes == sub_buffer.len() {
+                return true;
+            }
+        } else {
+            matching_bytes = 0;
+        }
+        return false;
+    });
+
+    if start_index.is_some() {
+        return Some(start_index.unwrap() + 1 - sub_buffer.len());
+    }
+
+    None
+}
+
+fn find_zlib_buffer_indexes(buffer: &Vec<u8>) -> (usize, usize) {
+    let zlib_start_index = find_matching_buffer(buffer, &ZLIB_START.to_vec(), 0);
+
+    if zlib_start_index.is_some() {
+        let zlib_start_index = zlib_start_index.unwrap();
+        let zlib_stop_index = find_matching_buffer(buffer, &ZLIB_STOP.to_vec(), zlib_start_index);
+
+        if zlib_stop_index.is_some() {
+            let zlib_stop_index = zlib_stop_index.unwrap();
+            return (zlib_start_index, zlib_stop_index);
+        } else {
+            panic!("No bytes matching end of zlib stream!");
+        }
+    } else {
+        panic!("No bytes matching start of zlib stream!");
+    }
+}
+
+fn extract_zlib_buffer_from_civ6_save(buffer: &Vec<u8>, output_buffer: &mut Vec<u8>) {
+    let (zlib_start_index, zlib_stop_index) = find_zlib_buffer_indexes(&buffer);
+
+    let zlib_buffer = Vec::from(&buffer[(zlib_start_index + 4)..(zlib_stop_index + 4)]);
+    
+    output_buffer.clear();
+
+    for x in 0..(zlib_buffer.len() / 65540 + 1) {
+        let start_offset = x * 65540;
+        let stop_offset = min(zlib_buffer.len(), start_offset + 65536);
+        let zlib_chunk = &zlib_buffer[start_offset..stop_offset];
+        output_buffer.extend_from_slice(zlib_chunk);
+    }
+}
 
 fn main() {
     let mut file = File::open("dev_solo.Civ6Save").unwrap();
-    let file_ref = Read::by_ref(&mut file);
-    let mut buffer = [0; 256];
+    let mut data: Vec<u8> = Vec::new();
+    file.read_to_end(&mut data).unwrap();
 
-    // Read "CIV6" header
-    file_ref.take(4).read(&mut buffer).unwrap();
-    println!("First 4 bytes: {}", String::from_utf8_lossy(&buffer[0..4]));
-    assert_eq!(&buffer[0..4], "CIV6".as_bytes());
+    assert_eq!(&data[0..4], "CIV6".as_bytes());
 
-    file_ref.take(4).read(&mut buffer).unwrap();
-    let mut current_chunk: Vec<u8> = Vec::from(&buffer[0..4]);
-    while file_ref.take(4).read(&mut buffer).is_ok() {
-        if buffer[0..4] == [1, 0, 0, 0] {
-            println!("END OF CHUNK?");
-            if current_chunk.len() > 0 {
-                println!("END OF CHUNK!");
-                println!("{:?}\n", current_chunk.hex_dump());
-                current_chunk = Vec::new();
-            } else if file_ref.take(10).read(&mut buffer).is_ok() && buffer[8..9] == ZLIB_HEADER {
-                // Todo: read ZLIB data
-                println!("ZLIB!")
-            } else {
-                panic!("Error!");
-            }
-        } else if buffer[0..4] == [32, 0, 0, 0] {
-            while file_ref.take(4).read(&mut buffer).is_ok() && buffer[0..4] != [1, 0, 0, 0] {
-                current_chunk.extend_from_slice(&buffer[0..4]);
-            }
-            println!("Data (Version maybe?):\n{:?}\n", current_chunk.hex_dump());
-            current_chunk = Vec::new();
-        } else if buffer[0..4] == [2, 0, 0, 0] {
-            current_chunk.extend_from_slice(&buffer[0..4]);
-            file_ref.take(8).read(&mut buffer).unwrap();
-            current_chunk.extend_from_slice(&buffer[0..8]);
-            println!("Data (unknown type):\n{:?}\n", current_chunk.hex_dump());
-            current_chunk = Vec::new();
-        } else {
-            // Read string
-            let mut title: Vec<u8> = Vec::from(&buffer[0..4]);
-            while file_ref.take(1).read(&mut buffer).is_ok() && buffer[0] != 0 {
-                title.push(buffer[0]);
-            }
-            println!("Data ({}):", String::from_utf8_lossy(&title).to_owned());
-            current_chunk = Vec::new();
-            while file_ref.take(4).read(&mut buffer).is_ok() && buffer[0..4] != [1, 0, 0, 0] {
-                current_chunk.extend_from_slice(&buffer[0..4]);
-            }
-            println!("{:?}\n", current_chunk.hex_dump());
-            current_chunk = Vec::new()
-        }
+    let mut compressed_data = Vec::new();
+    extract_zlib_buffer_from_civ6_save(&data, &mut compressed_data);
 
-        current_chunk.extend_from_slice(&buffer[0..4]);
+    let mut decompressor = Decompress::new(true);
+    let mut decompressed_data: Vec<u8> = Vec::new();
+    let result =
+        decompressor.decompress(&compressed_data, &mut decompressed_data, FlushDecompress::Sync);
+
+    if result.is_ok() {
+        println!("Decompression succeeded!");
+        println!("Writing result to a file...");
+
+        let mut data_out_file = File::create("dev_solo.bin").unwrap();
+        data_out_file.write_all(&decompressed_data).unwrap();
+    } else {
+        panic!("Decompression error:\n{:?}", result.unwrap_err());
     }
 }
